ff541688e430a0a9f6db63a723616cec55931662
Mohammed Taha Zubair <tahazubairmohammed@gmail.com>
2026-08-20 11:42:59 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(json());
+
+app.post("/api/v1/signup", (req, res) => {});
+
+app.post("/api/v1/signin", (req, res) => {});
+
+app.post("/api/v1/content", (req, res) => {});
+
+app.get("/api/v1/content", (req, res) => {});
+
+app.delete("/api/v1/content", (req, res) => {});
+
+app.post("/api/v1/brain/share", (req, res) => {});
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
